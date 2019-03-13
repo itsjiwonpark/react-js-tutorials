78ede7390d2068c79d91df8ee42177873a705f6d
박지원 <parkjiwon@bagjiwon-ui-MacBook-Pro-2.local>
2019-03-13 22:18:03 +0900

    first lecture

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
-import Layout from "./components/Layout";
+const reducer = function(state, action) {
+  if (action.type === "INC") {
+    return state + action.payload;
+  } else if (action.type === "DEC") {
+    return state - action.payload;
+  }
+  return state;
+};
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
+
+store.dispatch({ type: "INC", payload: 3 });
+store.dispatch({ type: "INC", payload: 1 });
+store.dispatch({ type: "INC", payload: 4 });
+store.dispatch({ type: "DEC", payload: 12 });
+store.dispatch({ type: "INC", payload: 11 });
