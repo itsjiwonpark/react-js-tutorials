d2b4832f6f22ae2d695480ca0a00ba2185822049
박지원 <parkjiwon@bagjiwon-ui-MacBook-Pro-2.local>
2019-03-15 19:37:00 +0900

    added some comments

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,22 +1,38 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const reducer = function(state, action) {
-  if (action.type === "INC") {
-    return state + action.payload;
-  } else if (action.type === "DEC") {
-    return state - action.payload;
+const userReducer = (state = {}, action) => {
+  //action은 전부 받는데 state가 user가 들어옴
+  switch (action.type) {
+    case "CHANGE_NAME": {
+      state = { ...state, name: action.payload };
+      break;
+    }
+    case "CHANGE_AGE": {
+      state = { ...state, age: action.payload };
+      break;
+    }
   }
   return state;
 };
 
-const store = createStore(reducer, 0);
+const tweetsReducer = (state = [], action) => {
+  // return state; 이렇게 아무것도 안리턴하면 error뜸
+  return state;
+};
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
+  // 비동기로 처리되나? 둘다 바뀐걸로 나오는데 근데 바뀐 걸 어떻게 알지? action이 dispatch돼서.. 두번 dispatch돼서
+  // 얘는 state를 비교해서 반응하는 애가 아니고 action이 dispatch됐을 때 콜백을 호출하는 녀석
   console.log("store changed!", store.getState());
 });
 
-store.dispatch({ type: "INC", payload: 3 });
-store.dispatch({ type: "INC", payload: 1 });
-store.dispatch({ type: "INC", payload: 4 });
-store.dispatch({ type: "DEC", payload: 12 });
-store.dispatch({ type: "INC", payload: 11 });
+store.dispatch({ type: "CHANGE_NAME", payload: "Will" });
+console.log("then what happens now");
+store.dispatch({ type: "CHANGE_AGE", payload: "35" });
