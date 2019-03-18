da4a8c015212c2f36bdac38a1740f7658c5b4542
박지원 <parkjiwon@bagjiwon-ui-MacBook-Pro-2.local>
2019-03-18 09:49:44 +0900

    redux-handmade-middleware

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
 
-import Layout from "./components/Layout";
+const reducer = (initialState = 0, action) => {
+  if (action.type === "INC") {
+    return ++initialState;
+  } else if (action.type === "DEC") {
+    return --initialState;
+  } else if (action.type === "E") {
+    throw new Error("AHHHHHH!!!!!");
+  }
+  return initialState;
+};
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const logger = store => next => action => {
+  console.log("action fired", action);
+  //  action.type = "DEC"; 중간에 수정도 가능함
+  next(action);
+};
+
+const error = store => next => action => {
+  try {
+    next(action);
+  } catch (e) {
+    console.log("AAHHHH!!", e);
+  }
+};
+
+const middleware = applyMiddleware(logger, error);
+
+const store = createStore(reducer, 1, middleware);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "DEC" });
+store.dispatch({ type: "E" });
